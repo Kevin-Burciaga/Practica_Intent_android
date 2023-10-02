4848dca499ca531f75457eadff5c73c3b1758533
Kevin-Burciaga <kvnbrcg@gmail.com>
2023-10-02 13:11:15 -0600

    intents, permisos diesño (texto en los botones, metodos y ultima version y countdown timer

--- app/src/main/java/com/example/ejemplo1event/MainActivity.java
+++ app/src/main/java/com/example/ejemplo1event/MainActivity.java
@@ -4,30 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-        Button B;
+        TextView txt;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-            B = (Button) findViewById(R.id.btn1);
-            B.setOnClickListener(this);
+            txt = (TextView) findViewById(R.id.txt2);
+            Intent i=new Intent(this, MainActivity2.class);
+
+            new CountDownTimer(10000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+                    txt.setText("Espere: " + millisUntilFinished / 1000);
+                }
+
+                public void onFinish() {
+                    txt.setText("Listo!");
+                    startActivity(i);
+                }
+            }.start();
+
 
         };
 
-    @Override
-    public void onClick(View view) {
-        Intent i=new Intent(this, MainActivity2.class);
-        startActivity(i);
 
-    }
+
 
 
 }
