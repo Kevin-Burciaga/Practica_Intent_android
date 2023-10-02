9c342dc39bd75bdd48847937e3a81fb165938cfc
Kevin-Burciaga <kvnbrcg@gmail.com>
2023-10-01 21:30:26 -0600

    intents, permisos diesño (texto en los botones, metodos y ultima version

--- app/src/main/java/com/example/ejemplo1event/MainActivity2.java
+++ app/src/main/java/com/example/ejemplo1event/MainActivity2.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
+import android.provider.MediaStore;
 import android.telecom.Call;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +15,7 @@ import android.widget.Button;
 import java.net.URL;
 
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
-
+    private final static String URL_Google = "https://www.google.com";
     Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8;
 
     @Override
@@ -23,29 +24,75 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_main2);
         btn1 = (Button) findViewById(R.id.btn1);
         btn1.setOnClickListener(this);
-        btn2 = (Button) findViewById(R.id.b2);
-        btn2.setOnClickListener(this);
+        btn7 = (Button) findViewById(R.id.btn7);
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri gmmIntentUri = Uri.parse("geo:25.496938,-103.412225");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                startActivity(mapIntent);
+            }
+        });
         btn3 = (Button) findViewById(R.id.btn3);
         btn3.setOnClickListener(this);
         btn4 = (Button) findViewById(R.id.btn4);
-        btn4.setOnClickListener(this);
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String searchTerm = "Chivas de Guadalajara";
+
+                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY, searchTerm);
+                startActivity(intent);
+            }
+        });
+        btn5 = (Button) findViewById(R.id.btn5);
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String videoUrl = "https://www.youtube.com/watch?v=80lwLvr1l_0&ab_channel=RichVagos";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(videoUrl));
+                intent.setPackage("com.google.android.youtube");
+                startActivity(intent);
+            }
+        });
+        btn6 = (Button) findViewById(R.id.btn6);
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(URL_Google));
+                startActivity(i);
+            }
+        });
+        btn2 = (Button) findViewById(R.id.b2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent, 1);
+                }
+            }
+        });
+        btn8 = (Button) findViewById(R.id.btn8);
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     @Override
     public void onClick(View view) {
         if (view == btn1){
             createAlarm("Alarma 1", 13, 30);
-        } else if (view == btn2) {
-            showMap(Uri.parse("geo:25.496938,-103.412225"));
         } else if (view == btn3) {
             llamada("8715074853");
-        } else if (view==btn4) {
-            searchWeb("Real Madrid");
-        } else if (view == btn5) {
-            playMedia(Uri.parse("https://www.youtube.com/watch?v=80lwLvr1l_0&ab_channel=RichVagos"));
-        } else if (view == btn6) {
-            abrirgoogle("https://uttorreon.mx/");
         }
+
     }
 
     public void createAlarm(String message, int hour, int minutes) {
@@ -66,40 +113,9 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    public void showMap(Uri geoLocation) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(geoLocation);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-    }
-
-    public void searchWeb(String query) {
-        Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-        intent.putExtra(SearchManager.QUERY, query);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-    }
 
 
-    public void playMedia(Uri file) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(file);
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-    }
 
-
-    public void abrirgoogle(String url) {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        intent.setPackage("com.android.chrome");
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        }
-    }
 
 
 }
